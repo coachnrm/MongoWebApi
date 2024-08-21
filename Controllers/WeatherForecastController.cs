7e5d3ce878662d016fd2ceb742a5a1e2d421ceeb
home <nuttapong_coach@hotmail.com>
2024-08-21 21:07:54 +0700

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoWebApi.Data;
+using MongoWebApi.Entities;
+
+namespace MongoWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("list")]
+        public async Task<IActionResult> GetList()
+        {
+            var myEntities = await context.WeatherForecast.Find(Builders<WeatherForecast>.Filter.Empty).ToListAsync();
+            return Ok(myEntities);
+        }
+
+        [HttpGet("single")]
+        public async Task<IActionResult> GetSingleG(Guid id)
+        {
+            var entities = await context.WeatherForecast.Find(Builders<WeatherForecast>.Filter.Empty).ToListAsync();
+            var myEntity = entities.FirstOrDefault(w => w.Id == id);
+            return Ok(myEntity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(WeatherForecast weather)
+        {
+            weather.Id = Guid.NewGuid();
+            await context.WeatherForecast.InsertOneAsync(weather);
+            return CreatedAtAction(nameof(Add), weather);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(WeatherForecast weather)
+        {
+            await context.WeatherForecast.ReplaceOneAsync(w => w.Id == weather.Id, weather);
+            return Ok(weather);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await context.WeatherForecast.DeleteOneAsync(w => w.Id == id);
+            return NoContent();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using MongoWebApi.Entities;
+using MongoDB.Driver;
+
+namespace MongoWebApi.Data
+{
+    public class AppDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDB");
+            var databaseName = configuration.GetConnectionString("DatabaseName");
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<WeatherForecast> WeatherForecast =>
+            _database.GetCollection<WeatherForecast>("WeatherForecastList");
+    }
+}
